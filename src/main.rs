f4b162ad1e769de9c1c354bb344370b72a2e0f3f
Aurélien Hamy <aurelienhamy@gmail.com>
2019-09-12 21:44:45 +0200

    Added main server features Get method partially implemented, error and log entirely

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,114 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-
 use log::*;
 use simple_logger;
+use std::fs;
+use std::sync::Arc;
+use threadpool::ThreadPool;
+use tiny_http::{Header, Method, Request, Response, Server};
 
-fn main() {
+const MAX_THREADS: usize = 4;
+const SERVER_ADDR: &str = "127.0.0.1:3000";
+
+fn main() -> Result<(), i32> {
+    // * Inits
+    //logger
     simple_logger::init().unwrap();
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    //server
+    let main_serv = Server::http(SERVER_ADDR).unwrap();
+    //shared thread server
+    let server = Arc::new(main_serv);
+    //pool
+    let pool = ThreadPool::new(MAX_THREADS);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    // * Threads creation
+    for worker_id in 0..MAX_THREADS {
+        let thread_server = Arc::clone(&server);
+        pool.execute(move || handle_server(thread_server, worker_id));
+    }
+    pool.join();
+    Ok(())
+}
 
-        handle_connection(stream);
+// * Handling the server: accept requests, handle them and wether respond or handle the error
+fn handle_server(thread_server: Arc<Server>, worker_id: usize) {
+    for request in thread_server.incoming_requests() {
+        debug!("New work for worker {}", worker_id);
+        match handle_request(request) {
+            Ok(()) => continue,
+            Err(e) => handle_error(e),
+        };
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 600];
+fn handle_request(request: Request) -> Result<(), ErrorType> {
+    match request.method() {
+        Method::Get => {
+            info!("GET request from {}", request.remote_addr());
+            handle_method_get(request)
+        }
+        uk_method => Err(ErrorType::UnknownMethod(uk_method.clone())),
+    }
+}
 
-    let s = stream.read(&mut buffer).unwrap();
+fn handle_method_get(request: Request) -> Result<(), ErrorType> {
+    let url = [
+        "server",
+        match match_path(request.url()) {
+            Some(u) => u,
+            // ! Implement 404 DOES NOT EXISTS in error data management
+            None => return Ok(()),
+        },
+        "index.html",
+    ]
+    .join("/");
 
-    info!(
-        "Request:\n\n{}\nRequest size: {}\n",
-        String::from_utf8_lossy(&buffer[..]),
-        s
-    );
+    trace!(target: "disks", "Serving page {}", &url);
+    let html_content = match fs::read_to_string(&url) {
+        Ok(c) => c,
+        Err(_) => return Err(ErrorType::FileNotFound(url)),
+    };
+
+    // * Create response
+    let content_type_header = Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap();
+    let mut response = Response::from_string(html_content);
+    response.add_header(content_type_header);
+    match request.respond(response) {
+        Ok(()) => Ok(()),
+        Err(_) => Err(ErrorType::CantRespond),
+    }
+}
+
+// * Path white_/black_listing
+fn match_path(url: &str) -> Option<&str> {
+    match url {
+        //? Authorized paths
+        "/" => Some("/"),
+        //? Blacklisted paths
+        "/favicon.ico" => None,
+        //? Other paths: 404 error
+        bad_path => {
+            warn!("404 : {:?} does not exists", bad_path);
+            Some("/errors/404")
+        }
+    }
+}
+
+// * Error handling
+enum ErrorType {
+    UnknownMethod(Method),
+    FileNotFound(String),
+    CantRespond,
+}
+
+fn handle_error(e: ErrorType) {
+    match e {
+        ErrorType::UnknownMethod(method) => {
+            warn!("unknown method {}, do nothing", method);
+        }
+        ErrorType::FileNotFound(file) => {
+            error!("file not found {}", file);
+        }
+        ErrorType::CantRespond => {
+            error!("can't respond, unknown error");
+        }
+    }
 }
