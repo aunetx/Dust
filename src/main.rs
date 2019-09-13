570d4b898da58cf0dc5d1b1aca2e89537ef14b8b
Aurélien Hamy <aurelienhamy@gmail.com>
2019-09-14 00:25:24 +0200

    Added complete mime_type and path support Changed way that serving files response works : now based on request and schemes

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ const SERVER_ADDR: &str = "127.0.0.1:3000";
 fn main() -> Result<(), i32> {
     // * Inits
     //logger
-    simple_logger::init().unwrap();
+    simple_logger::init_with_level(Level::Info).unwrap();
     //server
     let main_serv = Server::http(SERVER_ADDR).unwrap();
     //shared thread server
@@ -25,13 +25,14 @@ fn main() -> Result<(), i32> {
         pool.execute(move || handle_server(thread_server, worker_id));
     }
     pool.join();
+    println!("Finished");
     Ok(())
 }
 
 // * Handling the server: accept requests, handle them and wether respond or handle the error
-fn handle_server(thread_server: Arc<Server>, worker_id: usize) {
+fn handle_server(thread_server: Arc<Server>, _worker_id: usize) {
     for request in thread_server.incoming_requests() {
-        debug!("New work for worker {}", worker_id);
+        //debug!("New work for worker {}", worker_id);
         match handle_request(request) {
             Ok(()) => continue,
             Err(e) => handle_error(e),
@@ -42,11 +43,20 @@ fn handle_server(thread_server: Arc<Server>, worker_id: usize) {
 fn handle_request(request: Request) -> Result<(), ErrorType> {
     match request.method() {
         Method::Get => {
-            info!("GET request from {}", request.remote_addr());
+            info!(
+                "GET request from {} : {:?}",
+                request.remote_addr(),
+                request.url()
+            );
             handle_method_get(request)
         }
         Method::Put => {
-            info!("PUT request from {}", request.remote_addr());
+            info!(
+                target: "socket",
+                "PUT request from {} : {:?}",
+                request.remote_addr(),
+                request.url()
+            );
             handle_method_put(request)
         }
         uk_method => Err(ErrorType::UnknownMethod(uk_method.clone())),
@@ -55,20 +65,33 @@ fn handle_request(request: Request) -> Result<(), ErrorType> {
 
 // * Basic page and assets loading
 fn handle_method_get(request: Request) -> Result<(), ErrorType> {
-    let (path, code) = match match_path(request.url()) {
-        Some(r) => r,
-        None => return Ok(()),
+    let (path, code, mime_type) = match_path(request.url());
+    let url = if mime_type == "text/html" {
+        ["server", path, "index.html"].join("/")
+    } else {
+        ["server", path].join("/")
     };
-    let url = ["server", path].join("/");
 
     trace!(target: "disks", "Serving page {}", &url);
+
+    // * Checking for 404 error
     let html_content = match fs::read_to_string(&url) {
         Ok(c) => c,
-        Err(_) => return Err(ErrorType::FileNotFound(url)),
+        Err(_) => {
+            // If 404, try to open 404.html
+            warn!(target: "client", "404 : {:?} does not exists", url);
+            match fs::read_to_string("server/errors/404.html") {
+                Ok(c) => c,
+                Err(_) => return Err(ErrorType::FileNotFound("/errors/404.html".to_string())),
+            }
+        }
     };
 
     // * Create response
-    let content_type_header = Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap();
+    // Mime type
+    let content_type_header =
+        Header::from_bytes(&b"Content-Type"[..], mime_type.as_bytes()).unwrap();
+    // Content, headers and status code
     let response = Response::from_string(html_content)
         .with_header(content_type_header)
         .with_status_code(code);
@@ -77,6 +100,81 @@ fn handle_method_get(request: Request) -> Result<(), ErrorType> {
         Err(_) => Err(ErrorType::CantRespond),
     }
 }
+
+// * Path white_/black_listing
+fn match_path(url: &str) -> (&str, i32, &str) {
+    let (mime_type, is_unknown) = match match_mime_type(url) {
+        Ok(m) => (m, false),
+        Err(e) => {
+            handle_error(e);
+            ("text/html", true)
+        }
+    };
+    trace!("Mime type = {}", mime_type);
+
+    if is_unknown {
+        warn!(target: "client", "404 : {:?} does not exists", url);
+        ("/errors/404.html", 404, mime_type)
+    } else {
+        (url, 200, mime_type)
+    }
+
+    /*match url {
+        //? Authorized paths
+        "/" => ("/index.html", 200, mime_type),
+        //? Javascript paths
+        "/js/socket.js" => ("js/socket.js", 200, mime_type),
+        //? Images paths
+        "/res/logo.svg" => ("/res/logo.svg", 200, mime_type),
+        //? Blacklisted paths : DO NOT USE unless you are a lazy man (like for favicon.ico)
+        "/favicon.ico" => {
+            warn!(target: "client", "403: {:?} forbidden or blacklisted", url);
+            ("/errors/403.html", 403, mime_type)
+        }
+        //? Othbad_pather paths: 404 error
+        bad_path => {
+            warn!(target: "client", "404 : {:?} does not exists", bad_path);
+            ("/errors/404.html", 404, mime_type)
+        }
+    }*/
+}
+
+// * Match mime type
+fn match_mime_type(url: &str) -> Result<&str, ErrorType> {
+    let splitted_url: Vec<&str> = url.split('.').collect();
+    match splitted_url.len() {
+        1 => Ok("text/html"),
+        2 => {
+            match splitted_url[1] {
+                // Web
+                "html" => Ok("text/html"),
+                "js" => Ok("text/javascript"),
+                "css" => Ok("text/css"),
+                // Images
+                "apng" => Ok("image/apng"),
+                "bmp" => Ok("image/bmp"),
+                "gif" => Ok("image/gif"),
+                "ico" | "cur" => Ok("image/x-icon"),
+                "png" => Ok("image/png"),
+                "jpeg" | "jpg" => Ok("image/jpeg"),
+                "svg" => Ok("image/svg+xml"),
+                "tiff" | "tif" => Ok("image/tiff"),
+                "webp" => Ok("image/webp"),
+                // Fonts
+                "otf" => Ok("font/otf"),
+                "sfnt" => Ok("font/sfnt"),
+                "ttf" => Ok("font/ttf"),
+                "woff" => Ok("font/woff"),
+                "woff2" => Ok("font/woff2"),
+                // Unknown
+                m => Err(ErrorType::CantDecideMime(m.to_string())),
+            }
+        }
+        _ => Err(ErrorType::InvalidUrlSeparator),
+    }
+}
+
+// ! SOCKET
 
 // * Socket method : respond accordingly to that
 fn handle_method_put(request: Request) -> Result<(), ErrorType> {
@@ -95,41 +193,32 @@ fn handle_method_put(request: Request) -> Result<(), ErrorType> {
     }
 }
 
-// * Path white_/black_listing
-fn match_path(url: &str) -> Option<(&str, i32)> {
-    match url {
-        //? Authorized paths
-        "/" => Some(("/index.html", 200)),
-        //? Javascript paths
-        "/js/socket.js" => Some(("js/socket.js", 200)),
-        //? Blacklisted paths
-        "/favicon.ico" => None,
-        //? Other paths: 404 error
-        bad_path => {
-            warn!("404 : {:?} does not exists", bad_path);
-            Some(("/errors/404/index.html", 404))
-        }
-    }
-}
+// ! NOT SOCKET ANYMORE
 
 // * Error handling
 enum ErrorType {
     UnknownMethod(Method),
     FileNotFound(String),
     BadPutRequest(String),
+    CantDecideMime(String),
+    InvalidUrlSeparator,
     CantRespond,
 }
 
 fn handle_error(e: ErrorType) {
     match e {
         ErrorType::UnknownMethod(method) => {
-            warn!("unknown method {}, do nothing", method);
+            warn!("unknown method {:?}, do nothing", method);
         }
         ErrorType::FileNotFound(file) => {
-            error!("file not found {}", file);
+            error!("file not found {:?}", file);
         }
         ErrorType::BadPutRequest(request) => {
-            error!("PUT request does not cover message {}", request);
+            error!("PUT request does not cover message {:?}", request);
+        }
+        ErrorType::CantDecideMime(mime) => error!("Can't infer type for {:?}", mime),
+        ErrorType::InvalidUrlSeparator => {
+            error!("invalid url request number of separators, 404 error")
         }
         ErrorType::CantRespond => {
             error!("can't respond, unknown error");
