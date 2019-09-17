aff5efa9ec564c9f30d205b2cc0b2545ce408352
Aurélien Hamy <aurelienhamy@gmail.com>
2019-09-17 23:25:52 +0200

    Converted socket.js to typescript

--- server/js/socket.ts
+++ server/js/socket.ts
@@ -1,27 +1,33 @@
 function change(message) {
-    let socket = new Socket();
+    let socket = new Socket()
     socket.send(message, function (text) {
         document.getElementById("demo").innerHTML = text;
     });
 }
+
 class Socket {
-    constructor() {
-        this.ip = "http://" + location.host + "/socket/message";
-    }
+    ip: string;
+
     send(message, callback) {
         var request = new XMLHttpRequest();
+
         request.responseType = "json";
         request.open('PUT', this.ip, true);
+
         request.onreadystatechange = function () {
             if (request.readyState == 4) {
                 if (request.status == 200) {
                     callback(request.response.res);
-                }
-                else {
-                    console.log("Cannot retrieve socket answer : HTTP status code is " + request.status);
+                } else {
+                    console.log("Cannot retrieve socket answer : HTTP status code is " + request.status)
                 }
             }
-        };
+        }
+
         request.send();
+    }
+
+    constructor() {
+        this.ip = "http://" + location.host + "/socket/message";
     }
 }
