bb16039c2dd9ab7e24cc3b7e47e364a3eba0643e
Aurélien Hamy <aurelienhamy@gmail.com>
2019-09-17 20:40:18 +0200

    Created socket class

--- server/js/socket.js
+++ server/js/socket.js
@@ -1,21 +1,38 @@
 function change(message) {
-    send_message(message)
-}
 
-function send_message(message) {
-    var ip = "http://" + location.host + "/socket/message",
-        request = new XMLHttpRequest();
-
-    request.responseType = "json";
-    request.open('PUT', ip, true);
-
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-
-            document.getElementById("demo").innerHTML = request.response.res;
-
-        }
+    function callback(text) {
+        document.getElementById("demo").innerHTML = text;
     }
 
-    request.send();
+    socket.send(message, function (text) {
+        document.getElementById("demo").innerHTML = text;
+    });
+    console.log("Hello!");
+}
+
+
+var socket = {
+    ip_prepend: "http://",
+    ip_append: "/socket/message",
+    server_url: location.host,
+
+    send: async function (message, callback) {
+        var request = new XMLHttpRequest(),
+            ip = this.ip_prepend + this.server_url + this.ip_append;
+
+        request.responseType = "json";
+        request.open('PUT', ip, true);
+
+        request.onreadystatechange = function () {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    callback(request.response.res);
+                } else {
+                    console.log("Cannot retrieve socket answer : HTTP code " + request.status)
+                }
+            }
+        }
+
+        request.send();
+    }
 }
