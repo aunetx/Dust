766739b89f46017b4fb5b672d5b744610e255862
Aurélien Hamy <aurelienhamy@gmail.com>
2019-09-18 22:23:12 +0200

    Temporary Socket changes Added ts config files

---
+++ server/js/SOCKET.md
@@ -0,0 +1,11 @@
+# Socket documentation
+
+## Creating a realtime socket connection
+
+This creates a new `Socket` object :
+
+```javascript
+socket = new Socket()
+```
+
+You can also create that object with custom server 
--- server/js/socket.ts
+++ server/js/socket.ts
@@ -1,33 +1,61 @@
-function change(message) {
-    let socket = new Socket()
-    socket.send(message, function (text) {
-        document.getElementById("demo").innerHTML = text;
+function change() {
+    let message = "Hello world!";
+    let socket = new SocketNoRt();
+
+    socket.onResponse(function (text) {
+        console.log(text);
     });
+
+    socket.send(message);
 }
 
-class Socket {
+class SocketNoRt {
     ip: string;
+    request: XMLHttpRequest;
 
-    send(message, callback) {
-        var request = new XMLHttpRequest();
+    send(message: string) {
+        this.request.send();
+    }
 
-        request.responseType = "json";
-        request.open('PUT', this.ip, true);
-
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    callback(request.response.res);
-                } else {
-                    console.log("Cannot retrieve socket answer : HTTP status code is " + request.status)
-                }
+    stateChanged(event) {
+        let req: XMLHttpRequest = event.target
+        console.log(this)
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                console.log("Well done")
+                //soThis.on_response(req.response.res);
+            } else {
+                console.log("Cannot retrieve socket answer : HTTP status code is " + req.status)
             }
         }
+    }
 
-        request.send();
+    // Response function set by user, BEFORE sending with onResponse(function(t) {...})
+    on_response: Function;
+    onResponse(callback: Function) {
+        this.on_response = callback;
     }
 
     constructor() {
         this.ip = "http://" + location.host + "/socket/message";
+        // Request init
+        this.request = new XMLHttpRequest();
+        this.request.responseType = "json";
+        this.request.open('PUT', this.ip, true);
+        this.request.onreadystatechange = this.stateChanged;
+    }
+}
+
+class SocketRt {
+    server: {
+        ip: string;
+    }
+    client: {
+        id: number;
+    }
+    request: XMLHttpRequest;
+
+    constructor() {
+        
     }
 }
